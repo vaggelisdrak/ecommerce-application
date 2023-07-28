8f78cc16290661025ba41c4a043b6189f5a98a9f
vaggelisdrak <v.drakontaeidis@gmail.com>
2023-07-29 02:19:35 +0300

    featured-products

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { groq } from "next-sanity"
 import { SanityProduct } from "@/config/inventory"
 import { ProductGallery } from "@/components/product-gallery"
 import { ProductInfo } from "@/components/product-info"
+import { FeaturedProducts } from "@/components/featured-products"
 
 interface Props {
   params: {
@@ -29,6 +30,24 @@ export default async function Page({ params }: Props) {
     "slug": slug.current
   }`)
 
+  const products =  await client.fetch<SanityProduct[]>(groq`*[_type == "product" && slug.current != "${params.slug}" && "${product.categories[0]}" in categories ]{
+    _id,
+    _createdAt,
+    "id": _id,
+    name,
+    sku,
+    images,
+    price,
+    currency,
+    description,
+    sizes,
+    categories,
+    colors,
+    "slug": slug.current
+  }`)
+
+  console.log(product.categories[0])
+
   return (
     <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
       <div className="mx-auto max-w-2xl lg:max-w-none">
@@ -38,6 +57,13 @@ export default async function Page({ params }: Props) {
           <ProductGallery product={product}/>
           {/* Product info */}
           <ProductInfo product={product}/>
+        </div>
+        <hr/><br/>
+        <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
+          <h1 className="text-3xl font-bold tracking-tight pl-1">Featured Products</h1>
+          <br/><br/>
+          <FeaturedProducts products={products}/>
+          <br/>
         </div>
       </div>
     </main>
